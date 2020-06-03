c10f4bdd84d20a1ff4965a2b3898412a55c5c8b1
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-06-03 15:52:41 +0900

    ポップアップ画面の雛形作成

--- src/Components/MapDetailCard/index.ts
+++ src/Components/MapDetailCard/index.ts
@@ -3,4 +3,21 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class MapDetailCard extends Vue {
 
+    private name: string = 'Name section';
+    private description: string = 'Desctiption section'
+
+    /**
+     * closeボタンを押すと詳細画面を閉じる
+     */
+    private close() {
+        // pass1
+    }
+
+    /**
+     * open mapボタンを押すとMap利用画面に移動
+     */
+    private openMap() {
+        // pass
+    }
+
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import MainView from '../components/MainView/index.vue';
+import MapDetailCard from '../components/MapDetailCard/index.vue';
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,11 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+    {
+        path: '/map-detail',
+        name: 'map-detail',
+        component: MapDetailCard,
     },
 ];
 
