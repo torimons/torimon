145cea6676f12606ab12d39228955fe4a774cdd3
imamura-gen <igen.kuel@gmail.com>
2020-06-18 18:44:05 +0900

    コメント反映

--- src/components/MapItem/index.ts
+++ src/components/MapItem/index.ts
@@ -3,10 +3,9 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class MapItem extends Vue {
     // 親からマップ名、作成者名を受けとり表示する
-    @Prop()
-    private mapName!: string;
-    @Prop()
-    private description!: string;
+    @Prop() private mapName!: string;
+    @Prop() private description!: string;
+    private userName!: string;
 
     /**
      * mapItemがクリックされると呼ばれ，mapListを非表示にする．
