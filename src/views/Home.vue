66c8e98bfcb4c1f7cb773bf85d0f969c8a7670b1
mirlosu <45028997+mirlosu@users.noreply.github.com>
2020-07-24 11:00:43 +0900

    v-contentをv-mainに変更(warning解消)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <Menu>
-        <v-content>
+        <v-main>
             <v-container
                 class="fill-height"
                 fluid
@@ -50,7 +50,7 @@
                     </v-col>
                 </v-row>
             </v-container>
-        </v-content>
+        </v-main>
     </Menu>
 </template>
 
