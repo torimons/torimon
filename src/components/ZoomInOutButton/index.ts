d85d3000e6d09b3df4d4f641e459a4570cab778f
mirlosu <45028997+mirlosu@users.noreply.github.com>
2020-09-02 20:12:24 +0900

    ZoomInOutButtonコンポーネントの作成(未使用)

---
+++ src/components/ZoomInOutButton/index.ts
@@ -0,0 +1,18 @@
+import { Component, Vue, Watch} from 'vue-property-decorator';
+
+@Component
+export default class ZoomInOutButton extends Vue {
+    /**
+     * zoom_inボタンを押した時にzoominする
+     */
+    private zoomIn() {
+            this.$emit('zoomIn');
+    }
+
+    /**
+     * zoom_outボタンを押した時にzoomoutする
+     */
+    private zoomOut() {
+            this.$emit('zoomOut');
+    }
+}
---
+++ src/components/ZoomInOutButton/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id='zoomInOut'>
+        <v-btn id="zoom-in"
+            absolute
+            bottom
+            right
+            fab
+            small
+            @click="zoomIn"
+            @dblclick.stop=";"
+        >
+            <v-icon>zoom_in</v-icon>
+        </v-btn>
+        <v-btn id="zoom-out"
+            absolute
+            bottom
+            right
+            fab
+            small
+            @click="zoomOut"
+            @dblclick.stop=";"
+        >
+            <v-icon>zoom_out</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script lang='ts' src='./index.ts'/>
+
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+html,
+body,
+#zoom-in {
+    bottom: 120px;
+    z-index: 1000;
+}
+#zoom-out {
+    bottom: 120px;
+    z-index: 1000;
+}
+</style>
